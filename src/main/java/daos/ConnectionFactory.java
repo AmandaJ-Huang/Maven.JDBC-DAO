b9c164e58aae10b4cfd4c189fac1479c4f0c2215
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-09 00:29:18 -0500

    fixed SQLSyntaxErrorException-jdbc connection unable to recognize table, changed table name in MySQL from MyFaveMovies to movies

--- src/main/java/daos/ConnectionFactory.java
+++ src/main/java/daos/ConnectionFactory.java
@@ -1,15 +1,14 @@
 package daos;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public static final String URL = "jdbc:mysql://localhost:3306/MyFaveMovies";
-    public static final String USER = "rick";
-    public static final String PASS = "rick123";
+    public static final String URL = "jdbc:mysql://localhost:3306/movies";
+    public static final String USER = "root";
+    public static final String PASS = "zipcode0";
 
     /**
      * Get connection to database
@@ -17,19 +16,19 @@ public class ConnectionFactory {
     */
     public static Connection getConnection() {
         try {
-//            DriverManager.registerDriver(new Driver());
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException ex) {
-            throw new RuntimeException("Error connecting to the database.");
+            ex.printStackTrace();
         }
+        throw new RuntimeException();
     }
 
     /**
      * Test Connection
      */
 
-//    public static void main(String[] args) {
-//        Connection connection = ConnectionFactory.getConnection();
-//    }
+    public static void main(String[] args) {
+        Connection connection = ConnectionFactory.getConnection();
+    }
 
 }
